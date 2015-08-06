339bc6a2a12252d1b52d5e9afd578391cf365074
madeleyne vaca <t-mavac@microsoft.com>
2015-08-06 11:17:22 -0700

    switch to NDEF messages to get around an issue (file doesnt open in app)

--- app/src/main/java/com/codeu/amwyz/ct/NFCButtonFragment.java
+++ app/src/main/java/com/codeu/amwyz/ct/NFCButtonFragment.java
@@ -2,30 +2,31 @@ package com.codeu.amwyz.ct;
 
 import android.app.Fragment;
 import android.content.Intent;
-import android.net.Uri;
+import android.content.SharedPreferences;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
 import android.nfc.NfcAdapter;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.provider.Settings;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.OutputStreamWriter;
-
 /**
  * Created by t-mavac on 8/4/2015.
  */
 public class NFCButtonFragment extends Fragment {
     private NfcAdapter nfcAdapter;
+    SharedPreferences prefs;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState){
         View view = inflater.inflate(R.layout.nfc_button_fragment, container, false);
+        prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this.getActivity());
         Button shareButton = (Button) view.findViewById(R.id.button);
         shareButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,8 +39,6 @@ public class NFCButtonFragment extends Fragment {
     }
 
     public void testSendFile(View view) {
-        nfcAdapter = NfcAdapter.getDefaultAdapter(this.getActivity());
-
         // Check whether NFC is enabled on device
         if(!nfcAdapter.isEnabled()){
             // NFC is disabled, show the settings UI
@@ -57,26 +56,25 @@ public class NFCButtonFragment extends Fragment {
             startActivity(new Intent(Settings.ACTION_NFCSHARING_SETTINGS));
         } else {
             // NFC and Android Beam both are enabled
+            String userParseId = prefs.getString("user_id",null);
+            NdefMessage userJSON = new NdefMessage(new NdefRecord[] { NdefRecord.createMime("text/*", userParseId.getBytes())});
 
-            // File to be transferred
-            //for current testing purposes this is just the icon for the app
-            String fileName = getResources().getResourceName(R.mipmap.ic_launcher);
-
+            nfcAdapter.setNdefPushMessage(userJSON, this.getActivity());
             // Create a new file using the specified directory and name
-            try {
-                File fileToTransfer = new File("/sdcard/testFile.txt");
-                fileToTransfer.createNewFile();
-                fileToTransfer.setReadable(true, false);
-                FileOutputStream fOut = new FileOutputStream(fileToTransfer);
-                OutputStreamWriter myOutWriter = new OutputStreamWriter(fOut);
-                myOutWriter.append("test");
-                myOutWriter.close();
-                fOut.close();
-                nfcAdapter.setBeamPushUris(
-                        new Uri[]{Uri.fromFile(fileToTransfer)}, this.getActivity());
-            } catch (Exception e) {
-                Log.e("ERRR", "Could not create file", e);
-            }
+//            try {
+//                File fileToTransfer = new File("/sdcard/testFile.txt");
+//                fileToTransfer.createNewFile();
+//                fileToTransfer.setReadable(true, false);
+//                FileOutputStream fOut = new FileOutputStream(fileToTransfer);
+//                OutputStreamWriter myOutWriter = new OutputStreamWriter(fOut);
+//                myOutWriter.append("test");
+//                myOutWriter.close();
+//                fOut.close();
+//                nfcAdapter.setBeamPushUris(
+//                        new Uri[]{Uri.fromFile(fileToTransfer)}, this.getActivity());
+//            } catch (Exception e) {
+//                Log.e("ERRR", "Could not create file", e);
+//            }
 
         }
     }
