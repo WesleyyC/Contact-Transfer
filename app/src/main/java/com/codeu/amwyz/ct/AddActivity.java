715c5c8cfedd45b32da59d37d3d90d8e3baa8ae8
maye-vaca <madeleyne.vaca@gmail.com>
2015-08-11 00:06:55 -0400

    implement intent filter for addActivity to get the incoming Ndef message

--- app/src/main/java/com/codeu/amwyz/ct/AddActivity.java
+++ app/src/main/java/com/codeu/amwyz/ct/AddActivity.java
@@ -2,12 +2,18 @@ package com.codeu.amwyz.ct;
 
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.nfc.NdefMessage;
+import android.nfc.NfcAdapter;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.content.IntentFilter;
+import android.widget.Toast;
 
 /**
  * Created by goodautumn on 8/3/2015.
@@ -15,6 +21,9 @@ import android.view.View;
 public class AddActivity extends ActionBarActivity{
     FragmentManager fragmentManager = getFragmentManager();
     FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+    NfcAdapter mNfcAdapter;
+    PendingIntent mNfcPendingIntent;
+    IntentFilter[] mNdefExchangeFilters;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -25,6 +34,17 @@ public class AddActivity extends ActionBarActivity{
         QrEncodeButtonFragment qrEncodeButtonFragment = new QrEncodeButtonFragment();
         fragmentTransaction.add(R.id.bottom_add_fragment_container, qrEncodeButtonFragment);
         fragmentTransaction.commit();
+        mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
+
+        mNfcPendingIntent = PendingIntent.getActivity(this, 0, new Intent(this,
+                getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP
+                | Intent.FLAG_ACTIVITY_CLEAR_TOP), 0);
+
+
+        IntentFilter nfcIntent = new IntentFilter(NfcAdapter.ACTION_NDEF_DISCOVERED);
+        nfcIntent.addDataScheme("text/plain");
+        mNdefExchangeFilters = new IntentFilter[] { nfcIntent };
+
     }
 
     @Override
@@ -55,5 +75,56 @@ public class AddActivity extends ActionBarActivity{
                 break;
             }
         }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+//        mResumed = true;
+        // Sticky notes received from Android
+        if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(getIntent().getAction())) {
+            NdefMessage[] messages = getNdefMessages(getIntent());
+            byte[] payload = messages[0].getRecords()[0].getPayload();
+//            setNoteBody(new String(payload));
+            //add the contact by getting the parseId from the Ndef message
+            Utility.addContacts(this,payload.toString());
+            setIntent(new Intent()); // Consume this intent.
+        }
+//        enableNdefExchangeMode();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+//        mResumed = false;
+        mNfcAdapter.disableForegroundNdefPush(this);
+    }
+
+    //if the onResume method ends up not actually adding the contact,
+    //might need to add the contact from here...?
+//    @Override
+//    protected void onNewIntent(Intent intent) {
+//        // NDEF exchange mode
+//        if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(intent.getAction())) {
+//            NdefMessage[] msgs = getNdefMessages(intent);
+//            //promptForContent(msgs[0]);
+////            Utility.addContacts(this, );
+//        }
+//    }
+
+    public NdefMessage[] getNdefMessages(Intent intent) {
+        NdefMessage[] messages;
+        Parcelable[] rawMsgs = intent.getParcelableArrayExtra(mNfcAdapter.EXTRA_NDEF_MESSAGES);
+        if (rawMsgs != null) {
+            messages = new NdefMessage[rawMsgs.length];
+            for (int i = 0; i < rawMsgs.length; i++) {
+                messages[i] = (NdefMessage) rawMsgs[i];
+            }
+            return messages;
+        }
+        else if (rawMsgs == null){
+            Toast.makeText(getApplicationContext(), "No NDEF Message Read", Toast.LENGTH_LONG).show();
+        }
+        return null;
     }
 }
